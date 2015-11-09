4525677152941701b4276b99d8d127fa563ad273
RKOONICHER <rkoonicher@iphone.man.co>
2015-11-09 16:48:21 -0500

    Adding content for home and adding controller checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get 'login' => 'static_pages#login'
+  get 'checkbox' => 'static_pages#checkbox'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
